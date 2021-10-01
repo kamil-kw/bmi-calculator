b1904b4c3d41a946ba47752ef5c1216f60052f50
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-10-01 10:11:00 +0000

    Initial commit, creating main function to run all functions

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def main():
+    """
+    Run all program functions
+    """
+
+print("Welcome in the BMI Calculator")
+main()
