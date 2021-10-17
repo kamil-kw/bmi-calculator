a970fd8536d57b35c3f2f01d897d270812b1368c
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-10-17 20:02:08 +0000

    Correcting code comments

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ def is_digit(user_input):
     Function is checking if value is a number
     return boolean True or False
     """
-    # if any characters is digit return boolean True else False
+    # If any characters is digit return boolean True else False
     if any(char.isdigit() for char in user_input):
         return True
     return False
@@ -14,7 +14,7 @@ def is_letter(user_input):
     Function is checking if value is a letter
     return boolean True or False
     """
-    # if any characters is letter -> return boolean True else False
+    # If any characters is letter -> return boolean True else False
     if any(char.isalpha() for char in user_input):
         return True
     return False
@@ -25,6 +25,7 @@ def is_char(user_input):
     Function is checking if input include any character
     return boolean True or False
     """
+    # Check lenght of input and if equal to zero return True
     if len(user_input) == 0:
         return True
     return False
@@ -32,15 +33,17 @@ def is_char(user_input):
 
 def is_special_char(user_input):
     """
-    Function is checking if input include any character
+    Function is checking if input include any special character
     return boolean True or False
     """
+    # Expected special character list
     symbols = {'~', ':', "'", '+', '[',
                '\\', '@', '^', '{', '%',
                '(', '-', '"', '*', '|', ',',
                '&', '<', '`', '}', '.', '_',
                '=', ']', '!', '>', ';', '?',
                '#', '$', ')', '/'}
+    # If symbol exist in user input return True
     if any(symbol in symbols for symbol in user_input):
         return True
     return False
@@ -51,24 +54,23 @@ def user_input_name():
     Function collecting a user name and use it globally
     and checking value if is string using is_digit() function
     """
-    # variable to use outcome globally in other functions
+    # Variable to use outcome globally in other functions
     global name
     # User name input
     name = input('\n\033[1;32;10mPlease enter your name:\n')
-    # if no characters in input -> message to user and repeat this function
+    # If no characters in input -> message to user and repeat this function
     if is_char(name) is True:
         print("\033[1;31;10mNo characters please type your name")
         user_input_name()
-    # else if character is digit -> message to user and repeat this function
+    # Else if character is digit -> message to user and repeat this function
     elif is_digit(name) is True:
         print("\033[1;31;10mPlease do not include digits in your name.")
         user_input_name()
-    # is special symbol -> message to user and repeat this function
+    # Is special symbol -> message to user and repeat this function
     elif is_special_char(name) is True:
         print("\033[1;31;10mPlease do not include special symbols.")
         user_input_name()
-
-    # else return name
+    # Else return name
     return name
 
 
@@ -77,29 +79,29 @@ def user_input_age():
     Function collecting a user age and checks if value is
     digit using is_digit() function
     """
-    # variable to use outcome globally in other functions
+    # Variable to use outcome globally in other functions
     global age
-    # variable to ensure PEP8 convention pass (amount of characters in line)
+    # Variable to ensure PEP8 convention pass (amount of characters in line)
     enter_age = " please enter your age:"
     # User age input
     age = input(f"\n\033[1;32;10mHi {name.capitalize()}{enter_age}\n")
-    # if no characters in input -> message to user and repeat this function
+    # If no characters in input -> message to user and repeat this function
     if is_char(age) is True:
         print("\033[1;31;10mNo characters please type your age")
         user_input_age()
-    # is special symbol -> message to user and repeat this function
+    # Is special symbol -> message to user and repeat this function
     elif is_special_char(age) is True:
         print("\033[1;31;10mPlease do not include special symbols.")
         user_input_age()
-    # if characters is letter -> message to user and repeat this function
+    # If characters is letter -> message to user and repeat this function
     elif is_letter(age) is True:
         print("\n\033[1;31;10mPlease do not include letter in your age.")
         user_input_age()
-    # if age is to high over 120 -> message to user and repeat this function
+    # If age is to high over 120 -> message to user and repeat this function
     elif int(age) >= 120:
         print("\n\033[1;31;10mIs this a correct age?")
         user_input_age()
-    # else return age
+    # Else return age
     return age
 
 
@@ -112,23 +114,23 @@ def choose_unit():
     enter = 'Enter unit of measurement by typing a number:\n'
     si = '1. The International System of Units (SI) cm or kg'
     usc = '2. United States Customary Units (USC) in or lbs\n'
-    # variables to use outcome globally in other functions
+    # Variables to use outcome globally in other functions
     global enter_weight
     global enter_height
     global unit_type
     # User measurement unit input
     unit_type = input(f"\n\033[1;32;10m{enter}\n{si}\n{usc}\n")
-    # if user typed 1 -> message about choice - and return comment
+    # If user typed 1 -> message about choice - and return comment
     if unit_type == "1":
         print(f"\n{name.capitalize()} the units you have choosen is SI\n")
         enter_weight = "please enter your weight in kg"
         enter_height = "please enter your height in cm"
-    # if user typed 2 -> message about choice - and return comment
+    # If user typed 2 -> message about choice - and return comment
     elif unit_type == "2":
         print(f"\n{name.capitalize()} the units you have choosen is USC\n")
         enter_weight = "please enter your weight in lbs"
         enter_height = "please enter your height in in"
-    # if user typed other value -> message about choice - and repeat function
+    # If user typed other value -> message about choice - and repeat function
     else:
         print("\033[1;31;10mPlease choose correct value 1 or 2\n")
         choose_unit()
@@ -139,23 +141,23 @@ def user_input_weight():
     Function collecting a user weight
     and checking value if is digit using is_digit()
     """
-    # variable to use outcome globally in other functions
+    # Variable to use outcome globally in other functions
     global weight
     # User weight input
     weight = input(f"\n\033[1;32;10mHi {name.capitalize()} {enter_weight}:\n")
-    # if no characters in input -> message to user and repeat this function
+    # If no characters in input -> message to user and repeat this function
     if is_char(weight) is True:
         print("\033[1;31;10mNo characters please type your weight")
         user_input_weight()
-    # is special symbol -> message to user and repeat this function
+    # Is special symbol -> message to user and repeat this function
     elif is_special_char(weight) is True:
         print("\033[1;31;10mPlease do not include special symbols.")
         user_input_weight()
-    # if characters is letter -> message to user and repeat this function
+    # If characters is letter -> message to user and repeat this function
     elif is_letter(weight) is True:
         print("\n\033[1;31;10mPlease do not include letter in your weight.")
         user_input_weight()
-    # else return weight
+    # Else return weight
     return weight
 
 
@@ -168,11 +170,11 @@ def user_input_height():
     global height
     # User height input
     height = input(f"\n\033[1;32;10mHi {name.capitalize()} {enter_height}:\n")
-    # if no characters in input -> message to user and repeat this function
+    # If no characters in input -> message to user and repeat this function
     if is_char(height) is True:
         print("\033[1;31;10mNo characters please type your height")
         user_input_height()
-    # is special symbol -> message to user and repeat this function
+    # Is special symbol -> message to user and repeat this function
     elif is_special_char(height) is True:
         print("\033[1;31;10mPlease do not include special symbols.")
         user_input_height()
@@ -201,9 +203,9 @@ def bmi_categories():
     """
     Function will describe bmi score based on age
     """
-    # variable to ensure PEP8 convention pass (amount of characters in line)
+    # Variable to ensure PEP8 convention pass (amount of characters in line)
     your_bmi = "your BMI Category is "
-    # result to user in age below 18
+    # Result to user in age below 18
     if int(age) < 18:
         """
         If age of user is below 18
@@ -243,18 +245,21 @@ def restart_calculator():
     """
     This function will receive user input y - continue or n - exit
     """
-    # variables to ensure PEP8 convention pass (amount of characters in line)
+    # Variables to ensure PEP8 convention pass (amount of characters in line)
     repeat = "Do you want to repeat calculations?"
     yes = "Press 'y' than 'enter' to continue calculations"
     no = "Press 'n' than 'enter' to quit calculations"
     # User decision y or n input
     restart = input(f"\033[1;32;10m\n{repeat}\n{yes}\n{no}\n")
+    # If user decide to restart calculation
     if restart == 'y':
         print('\n\033[1;33;10mCalculate again\n')
         main()
+    # If user decide to end calculation
     elif restart == 'n':
         print('\n\033[1;33;10mSee you next time')
         exit()
+    # Else type incorrect value
     else:
         print("\n\033[1;31;10mIncorrect character, please type 'y' or 'n'")
         restart_calculator()
@@ -274,6 +279,7 @@ def main():
     restart_calculator()
 
 
+# Welcome logo
 print("\n\033[1;34;10mWelcome in the BMI Calculator\n")
 print("\033[1;31;10m*******************************************\n")
 print("\033[1;31;10m  #       #####    ##    ##   ##        #")
@@ -283,6 +289,9 @@ print("\033[1;35;10m #        ##  ##   ##    ##   ##         #")
 print("\033[1;33;10m  #       #####    ##    ##   ##        #\n")
 print("*******************************************")
 print("\n\033[1;34;10mBody Mass Index for Everyone")
+
+
+# Program description, functionality explanation
 print("\n\033[1;35;10mUse this calculator to check your body mass index (BMI)")
 print("Type your age and body details weight & height than program will")
 print("calculate your BMI index based on details provided")
